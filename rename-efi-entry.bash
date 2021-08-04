d9286d937e53c0ce001191034d7bd9554bd62fd2
Sergey Ushakov <s-n-ushakov@yandex.ru>
2021-08-04 17:56:15 +0300

    implement wildcard EFI label matching
    also: various improvements and updates in comments and README.adoc

--- rename-efi-entry.bash
+++ rename-efi-entry.bash
@@ -2,7 +2,7 @@
 #=======================================================================================================================
 # Script to rename EFI boot entries
 #
-# This script renames an existing EFI boot entry by deleting it and re-creating with the required label.
+# This script renames an existing EFI boot entry by deleting it and re-creating with a required label.
 # See https://bbs.archlinux.org/viewtopic.php?pid=1215494#p1215494 .
 # See https://bbs.archlinux.org/viewtopic.php?pid=1083424#p1083424 .
 # See https://www.linuxbabe.com/command-line/how-to-use-linux-efibootmgr-examples .
@@ -29,6 +29,8 @@ print_usage () {
   echo "  sudo $0 ubuntu 'ubuntu 18.04'"
   echo Example:
   echo "  sudo $0 ubuntu 'ubuntu 18.04' 0001"
+  echo Example:
+  echo "  sudo $0 '*' 'ubuntu 18.04' 0001"
 }
 
 # function to print usage and EFI data
@@ -38,6 +40,8 @@ print_usage_and_efi_data () {
   echo Current EFI data:
   efibootmgr --verbose
 }
+
+# script start ---------------------------------------------------------------------------------------------------------
 
 # check whether we run as root
 if [[ $EUID -ne 0 ]]; then
@@ -49,11 +53,11 @@ fi
 # check command line arguments
 if [ -z "$2" ] ; then
   if [ -z "$1" ] ; then
-    echo "$0 : ERROR : no existing EFI label specified to be renamed."
+    echo "$0 : ERROR : no existing EFI label specified to be renamed"
     print_usage_and_efi_data
     exit 1
   else
-    echo "$0 : ERROR : no new EFI label specified."
+    echo "$0 : ERROR : no new EFI label specified"
     print_usage
     exit 1
   fi
@@ -88,7 +92,7 @@ declare -A partitions
 # loop over all disk devices
 for disk_name in "${disk_names[@]}" ; do
   # obtain partition data as long text, suppressing possible error messages in stderr,
-  #   like "sfdisk: /dev/sdb: does not contain a recognized partition table"
+  #   e.g. "sfdisk: /dev/sdb: does not contain a recognized partition table"
   # NOTE sfdisk call requires `sudo`
   partition_data_all=$(sfdisk -d /dev/$disk_name 2>/dev/null | grep ': start=')
 
@@ -122,17 +126,17 @@ readarray -t efi_data_array <<<"$efi_data_all"
 for efi_data_line in "${efi_data_array[@]}" ; do
   if [[ $efi_data_line =~ ^Boot([[:xdigit:]]{4})\*?[[:blank:]]+(.+)[[:blank:]]+HD\(([[:digit:]]+),[^,]+,([^,]+)[^\)]+\)/File\(([^\)]+)\) ]] ; then
     label="${BASH_REMATCH[2]}"
-    if [ "$label" = "$old_label" ] ; then
-      if [ -z "$target_bootnum" ] ; then
+    if [ "$label" = "$old_label" ] || [ "$old_label" = '*' ] ; then
+      if [ -z "$target_bootnum" ] ; then   # no `bootnum` match or candidate found yet
         if [ -z "$old_bootnum" ] || [ ${BASH_REMATCH[1]} = "$old_bootnum" ] ; then
           target_bootnum=${BASH_REMATCH[1]}
           target_part=${BASH_REMATCH[3]}
           target_uuid=${BASH_REMATCH[4]}
           target_loader=${BASH_REMATCH[5]}
         fi
-      else
+      else   # `bootnum` match or candidate already found
         if [ -z "$old_bootnum" ] ; then
-          echo "ERROR: more than one boot entry found with label as '$old_label': $target_bootnum and ${BASH_REMATCH[1]};"
+          echo "ERROR: more than one boot entry found with label matching '$old_label': $target_bootnum and ${BASH_REMATCH[1]};"
           echo "       please use optional 'bootnum' command line argument to resolve this ambiguity."
           exit 1
         fi
@@ -155,7 +159,7 @@ if [ -z "$device_for_uuid" ] ; then
   exit 1
 fi
 
-# verify that device/partition name matches expected pattern;
+# verify that device/partition name matches the expected pattern;
 # the following partition name patterns/samples are recognized:
 # - /dev/sda1
 # - /dev/nvme0n1p1
@@ -175,22 +179,22 @@ if [[ $device_part != $target_part ]] ; then
   exit 1
 fi
 
-# prepare the efibootmgr commands to be executed
+# prepare efibootmgr commands to be executed
 printf -v escaped_loader "%q" $target_loader
 efi_command_1="efibootmgr --bootnum $target_bootnum --delete-bootnum"
 efi_command_2="efibootmgr --create --disk $device_name --part $target_part --label '$new_label' --loader $escaped_loader"
 
-# obtain final user consent and make the modifications
+# obtain final user consent and apply modifications
 echo The following commands are about to be executed:
 echo "  $efi_command_1"
 echo "  $efi_command_2"
 read -p "Execute these commands? [y/N] " -n 1 -r
-echo   # terminate the line
+echo   # terminate the shell UI line
 if [[ $REPLY =~ ^[Yy]$ ]] ; then
   echo "... executing \`$efi_command_1\` ..."
   eval $efi_command_1
   echo "... executing \`$efi_command_2\` ..."
   eval $efi_command_2
 else
-  echo "command execution aborted"
+  echo "$0 : INFO : command execution aborted"
 fi
